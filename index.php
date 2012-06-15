8bee10da4f1e215664cf1f3449abede71e7fe15f
Ivan Nawrath <inawrath@informaticos.com>
2012-06-14 23:55:01 -0400

    Primera clase Taller PHP

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include 'controlador/indexControlador.php';
+
+$consulta = new indexControlador();
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Inicio</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
